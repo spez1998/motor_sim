35d743e1ac8e7e216fee519e50ad0f80fb06a509
spez1998 <samman30@live.com>
2022-02-23 13:49:03 +0000

    Better formatting

--- motor3_vars.m
+++ motor3_vars.m
@@ -1,9 +1,9 @@
-% Environment properties
+%% Environment properties
 Vdc = 400; % Inverter DC voltage
 fs = 10e4; % Frequency of supply
 Ts = 5e-7; % Sample time
 
-% Motor properties
+%% Motor properties
 p = 3;            % Pole pairs
 psi_mfl = 0.3119; % Magnetic flux linkage, psi
 Ls = 1e-3;        % Stator self-inductance per phase
@@ -12,7 +12,7 @@ Ms = 1e-3;        % Stator mutual inductance
 Kt = 50;          % Torque constant 
 Rs = 5;           % Not sure
 
-% Control loop properties
+%% Control loop properties
 Lphph = 25.19e-3;    % Phase-to-phase inductance
 Rphph = 4.37;        % Phase-to-phase resistance
 Ls_dq = Lphph * 1.5; % Synchronous inductance
