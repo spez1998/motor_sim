56a8f98be41b3f839c3e048124ad5b8d6b486a05
spez1998 <samman30@live.com>
2022-02-23 13:24:54 +0000

    Model structure complete

--- motor3_vars.m
+++ motor3_vars.m
@@ -1,10 +1,22 @@
-Vdc = 400;
-p = 3;
-psi_mfl = 0.3119;
-Ls = 1e-3;
-Lm = 2e-3;
-Ms = 1e-3;
-Kt = 50;
-Rs = 5;
-fs = 10e4;
-Ts = 5e-5;
+% Environment properties
+Vdc = 400; % Inverter DC voltage
+fs = 10e4; % Frequency of supply
+Ts = 5e-7; % Sample time
+
+% Motor properties
+p = 3;            % Pole pairs
+psi_mfl = 0.3119; % Magnetic flux linkage, psi
+Ls = 1e-3;        % Stator self-inductance per phase
+Lm = 2e-3;        % Stator inductance fluctuation
+Ms = 1e-3;        % Stator mutual inductance
+Kt = 50;          % Torque constant 
+Rs = 5;           % Not sure
+
+% Control loop properties
+Lphph = 25.19e-3;    % Phase-to-phase inductance
+Rphph = 4.37;        % Phase-to-phase resistance
+Ls_dq = Lphph * 1.5; % Synchronous inductance
+wcc = 200;           % Closed-loop bandwidth
+Kp_dq = Ls_dq * wcc; % dq controller proportional gain
+Ki_dq = Rphph * wcc; % dq controller integral gain
+
