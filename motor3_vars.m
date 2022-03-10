04cb06abeaa4ebf2b7f0f1b7cd25b5190c9795e4
spez1998 <samman30@live.com>
2022-03-10 12:23:58 +0000

    Variables for 115u3 PMSM FOC model

--- motor3_vars.m
+++ motor3_vars.m
@@ -1,7 +1,8 @@
 %% Environment properties
-Vdc = 400; % Inverter DC voltage
-fs = 10e4; % Frequency of supply
-Ts = 5e-7; % Sample time
+Vdc = 750;  % Inverter input DC voltage
+fs = 5e3;   % Inverter switching frequency
+Ts = 1e-6;  % Sample time
+Tsc = 1e-5; % Control loop sample time
 
 %% Motor properties
 p = 3;            % Pole pairs
@@ -9,14 +10,17 @@ psi_mfl = 0.3119; % Magnetic flux linkage, psi
 Ls = 1e-3;        % Stator self-inductance per phase
 Lm = 2e-3;        % Stator inductance fluctuation
 Ms = 1e-3;        % Stator mutual inductance
-Kt = 50;          % Torque constant 
-Rs = 5;           % Not sure
+Kt = 1.6;         % Torque constant
+Pmax = 2100;      % Maximum output power
+Tmax = 2.22;      % Maximum torque out
+J = 0.00077;      % Inertia
 
 %% Control loop properties
 Lphph = 25.19e-3;    % Phase-to-phase inductance
 Rphph = 4.37;        % Phase-to-phase resistance
+Rph   = Rphph/2;     % Per-phase resistance for delta-wound stator
 Ls_dq = Lphph * 1.5; % Synchronous inductance
-wcc = 200;           % Closed-loop bandwidth
+wcc = 2*pi*200;      % Closed-loop bandwidth
 Kp_dq = Ls_dq * wcc; % dq controller proportional gain
-Ki_dq = Rphph * wcc; % dq controller integral gain
+Ki_dq = Rph * wcc;   % dq controller integral gain
 
